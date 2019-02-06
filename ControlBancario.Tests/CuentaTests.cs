103c4e99815c005536124a6d59d08f47be5e7902
Beethoven Expedio Aquino Gonzalez <beethoven_aquino@ucne.edu.do>
2019-02-06 15:49:31 -0400

    Creacion del Test

---
+++ ControlBancario.Tests/CuentaTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BLL;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControlBancario.Tests
+{
+    [TestClass]
+    public class CuentaTests
+    {
+        [TestMethod]
+        public void Guardar()
+        {
+            RepositorioBase<Cuentas> repositorio = new RepositorioBase<Cuentas>();
+
+            Cuentas cuentas = new Cuentas();
+            cuentas.CuentaID = 0;
+
+            cuentas.Fecha = DateTime.Now;
+            cuentas.Nombre = "Clifford";
+            cuentas.Balance = 0;
+
+            Assert.IsTrue(repositorio.Guardar(cuentas));
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+            RepositorioBase<Cuentas> repositorio = new RepositorioBase<Cuentas>();
+
+            Cuentas cuentas = new Cuentas();
+            cuentas.CuentaID = 1;
+
+            cuentas.Fecha = DateTime.Now;
+            cuentas.Nombre = "Marieth";
+            cuentas.Balance = 0;
+
+            Assert.IsTrue(repositorio.Modificar(cuentas));
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            RepositorioBase<Cuentas> repositorio = new RepositorioBase<Cuentas>();
+            Assert.IsNotNull(repositorio.Eliminar(1));
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            RepositorioBase<Cuentas> repositorio = new RepositorioBase<Cuentas>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod]
+        public void GetList()
+        {
+            RepositorioBase<Cuentas> repositorio = new RepositorioBase<Cuentas>();
+            var lista = repositorio.GetList(x => true);
+            Assert.IsNotNull(lista);
+
+        }
+    }
+}
---
+++ ControlBancario.Tests/DepositoTests.cs
@@ -0,0 +1,71 @@
+﻿using BLL;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlBancario.Tests
+{
+
+    [TestClass]
+   public class DepositoTests
+    {
+        [TestMethod]
+        public void Guardar()
+        {
+            RepositorioBase<Deposito> repositorio = new RepositorioBase<Deposito>();
+
+            Deposito deposito = new Deposito();
+            deposito.DepositoID = 0;
+
+            deposito.Fecha = DateTime.Now;
+            deposito.CuentaID = 1;
+            deposito.Concepto = "Pago de Luz";
+            deposito.Monto = 500;
+
+            Assert.IsTrue(repositorio.Guardar(deposito));
+        }
+
+        [TestMethod]
+        public void Modificar()
+        {
+            RepositorioBase<Deposito> repositorio = new RepositorioBase<Deposito>();
+
+            Deposito deposito = new Deposito();
+            deposito.DepositoID = 1;
+
+            deposito.Fecha = DateTime.Now;
+            deposito.CuentaID = 1;
+            deposito.Concepto = "Pago de Luz";
+            deposito.Monto = 300;
+
+            Assert.IsTrue(repositorio.Guardar(deposito));
+        }
+
+        [TestMethod]
+        public void Eliminar()
+        {
+            RepositorioBase<Deposito> repositorio = new RepositorioBase<Deposito>();
+            Assert.IsNotNull(repositorio.Eliminar(1));
+        }
+
+        [TestMethod]
+        public void Buscar()
+        {
+            RepositorioBase<Deposito> repositorio = new RepositorioBase<Deposito>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+        }
+
+        [TestMethod]
+        public void GetList()
+        {
+            RepositorioBase<Deposito> repositorio = new RepositorioBase<Deposito>();
+            var lista = repositorio.GetList(x => true);
+            Assert.IsNotNull(lista);
+
+        }
+    }
+}
